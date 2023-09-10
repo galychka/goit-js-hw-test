a6ae8076d2335283d980a7173b6c31834b4abbd8
galychka <vgalychka@gmail.com>
2023-09-10 10:49:02 +0300

    error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_oF8LAyBgFl3f7g9EFkLmFYqgNea8M3zFMwBvpi4eyUvi5aVGCfGZZIizcp4KJD30";
 import SlimSelect from 'slim-select';
-import Notiflix, { Notify } from 'notiflix';
+import { Notify } from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './sass/cat-api';
 import 'slim-select/dist/slimselect.css';
+import './sass/_example.scss';
 
 const refs = {
-    container: document.querySelector('.container'),
     select: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
     error: document.querySelector('.error'),
     catInfo: document.querySelector('.cat-info'),
-    catPic: document.querySelector('.cat-info-desc-desc'),
-    catDesc: document.querySelector('.cat-info-desc-temp'),
+    catPic: document.querySelector('.cat-info-pict'),
+    catDesc: document.querySelector('.cat-info-desc'),
 };
 
 refs.select.addEventListener('change', onChangeSelect);
@@ -27,39 +29,39 @@ function renderSelect(breeds) {
     });
 }
 
+(function fetchBreedsRender() {
+    refs.loader.classList.remove('invisible');
+    fetchBreeds()
+        .then(breeds => renderSelect(breeds))
+        .catch(error => {
+            console.log(error);
+            Notify.failure('Oops! Something went wrong. Please try again later.');
+        })
+        .finally(() => {
+            refs.loader.classList.add('invisible');
+        });
+})();
+
 function renderDesc(breed) {
-    if (breed) {
-        const picture = `<img class="cat-picture" src="${breed.url}" alt="${breed.id}">`;
-        const descript = `<h2 class="cat-info-desc-title">${breed.name}</h2>
-            <p class="cat-info-desc-desc">${breed.description}</p>
-            <p class="cat-info-desc-temp"><b>Temperament:</b> ${breed.temperament}</p>`;
-        refs.catPic.innerHTML = picture;
-        refs.catDesc.innerHTML = descript;
-    } else {
-        refs.catPic.innerHTML = '';
-        refs.catDesc.innerHTML = 'Select a breed to see more information.';
-    }
+    const picture = `<img class="cat-picture" src="${breed.url}" alt="${breed.id}">`;
+    const descript = `<h2 class="cat-info-desc-title">${breed.breeds[0].name}</h2>
+            <p class="cat-info-desc-desc">${breed.breeds[0].description}</p>
+            <p class="cat-info-desc-temp"><b>Temperament:</b>${breed.breeds[0].temperament}</p>`;
+    refs.catPic.insertAdjacentHTML('beforeend', picture);
+    refs.catDesc.insertAdjacentHTML('beforeend', descript);
 }
 
-window.addEventListener('DOMContentLoaded', onChangeSelect);
-
 function onChangeSelect(e) {
-    refs.container.classList.remove('invisible');
+    refs.loader.classList.remove('invisible');
     refs.catInfo.innerHTML = '';
-
+    refs.catDesc.innerHTML = '';
     const breedId = e.target.value;
-
-    if (breedId) {
-        fetchCatByBreed(breedId)
-            .then(breed => {
-                renderDesc(breed);
-            })
-            .catch(error => {
-                console.error(error);
-                Notify.failure('Oops! Something went wrong. Please try again later.');
-            })
-            .finally(() => refs.container.classList.add('invisible'));
-    } else {
-        renderDesc(null);
-    }
+    console.log('breedId:', breedId);
+    fetchCatByBreed(breedId)
+        .then(breed => renderDesc(breed))
+        .catch(error => {
+            console.log(error);
+            Notify.failure('Oops! Something went wrong. Please try again later.');
+        })
+        .finally(() => refs.loader.classList.add('invisible'));
 }
